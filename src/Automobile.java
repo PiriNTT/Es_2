7b8f6a1b720313f0996a937c79b84bb472910f69
Giuseppe <giuseppeprudente20@gmail.com>
2023-06-07 12:45:58 +0200

    vaffa

--- src/Automobile.java
+++ src/Automobile.java
@@ -1,2 +1,49 @@
 public class Automobile {
+    public String marca;
+    public String modello;
+    public int anno;
+    public String colore;
+
+    public Automobile(String marca , String modello , int anno , String colore){
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.colore = colore;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public  void  mostraDettagli(){
+        System.out.println("Questi sono i dettsgli: " + this.marca + this.modello + this.anno + this.colore);
+    }
+
+    public String getColore() {
+        return colore;
+    }
+
+    public void setColore(String colore) {
+        this.colore = colore;
+    }
 }
