67b03050d1026d38d19e0e45765a2a490bf5a754
PiriNTT <Pietro.Giardina@nttdata.com>
2023-06-07 12:37:40 +0200

    Scrittura main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Automobile auto1 = new Automobile("Ferrari", "488 GBT", 2022, "Rosso");
+        Automobile auto2 = new Automobile("BMW", "X5", 2001, "Nero");
+        auto1.mostraDettagli();
+        auto2.mostraDettagli();
+        auto2.setColore("Rosso");
+        auto2.mostraDettagli();
     }
 }
